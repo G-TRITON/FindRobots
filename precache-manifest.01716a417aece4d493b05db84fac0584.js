7a53b9a7f231ff1c234aca76c95cfd2e679584a2
G-TRITON <braj85216@gmail.com>
2020-07-21 17:15:00 +0530

    Updates

---
+++ precache-manifest.01716a417aece4d493b05db84fac0584.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0006ea70b54b006b944b46c0b7ed7fd4",
+    "url": "/FindRobots/index.html"
+  },
+  {
+    "revision": "ec23c092ffb2b532aa2f",
+    "url": "/FindRobots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "617eb1a9c348596c286e",
+    "url": "/FindRobots/static/css/main.6243c195.chunk.css"
+  },
+  {
+    "revision": "ec23c092ffb2b532aa2f",
+    "url": "/FindRobots/static/js/2.89f77d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FindRobots/static/js/2.89f77d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617eb1a9c348596c286e",
+    "url": "/FindRobots/static/js/main.bc851502.chunk.js"
+  },
+  {
+    "revision": "995ccf0f5b4cc08cd632",
+    "url": "/FindRobots/static/js/runtime-main.b1a38832.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/FindRobots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
